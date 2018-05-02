c79315ffe8e43ee8514a54fafc8510b07b8ba06a
Jasper Lammering <jlammering@me.com>
2018-05-02 17:33:21 +0200

    kommentiert und 1e

--- Blatt2/aufgabe1/aufgabe1.cpp
+++ Blatt2/aufgabe1/aufgabe1.cpp
@@ -17,47 +17,47 @@
 
 using namespace std;
 
-class RNG{
+class RNG{ //Oberklasse um die Funktion abspeichern für beide Generatorklassen schreiben zu können
 public:
   virtual double getLittleRnd() = 0;
 };
 
-class LCG : public RNG{
+class LCG : public RNG{ //erbt von RNG
   private:
     uint32_t r_n, a, c, m;
   public:
-    LCG(uint32_t r_0_eingabe, uint32_t a_eingabe, uint32_t c_eingabe, uint32_t m_eingabe){
+    LCG(uint32_t r_0_eingabe, uint32_t a_eingabe, uint32_t c_eingabe, uint32_t m_eingabe){//Konstruktor
       r_n = r_0_eingabe;
       a = a_eingabe;
       c = c_eingabe;
       m = m_eingabe;
     }
-    uint32_t getRnd(){
+    uint32_t getRnd(){//ganzzahlige Zufallszahl erhalten
       r_n = (a*r_n + c)%m;
       return r_n;
     }
-    double getLittleRnd(){
+    double getLittleRnd(){//Zufallszahl zwischen 0 und 1 erhalten
       return (getRnd()/(double)m);
     }
 };
 
-class XORshift : public RNG{
+class XORshift : public RNG{ // erbt auch von RNG
   private:
     uint16_t a, b, c, y;
   public:
-    XORshift(uint16_t a_eingabe, uint16_t b_eingabe, uint16_t c_eingabe, uint16_t y_eingabe){
+    XORshift(uint16_t a_eingabe, uint16_t b_eingabe, uint16_t c_eingabe, uint16_t y_eingabe){//Konstruktor
       a = a_eingabe;
       b = b_eingabe;
       c = c_eingabe;
       y = y_eingabe;
     }
 
-    uint16_t getRnd(){
+    uint16_t getRnd(){//ganzzahlige Zufallszahl erhlaten nach XORshift
       y^=(y<<a);
       y^=(y>>b);
       return (y^=(y<<c));
     }
-    double getLittleRnd(){
+    double getLittleRnd(){//Zufallszahl zwischen 0 und 1 durch Division der Ganzzahligen durch das Maximum des 16-bit-Integer
       return (double)getRnd()/(double)(numeric_limits<uint16_t>::max());
     }
 };
@@ -72,10 +72,10 @@ void abspeichern(string dateiname, RNG& generator, uint32_t anzahlWerte){
 	myfile.close();
 }
 
-int testPeriod(XORshift& rng){
-  int seed = rng.getRnd();
-  int periode = 0;
-  for (; periode != seed; periode++) {}
+uint16_t testPeriod(XORshift& rng){
+  uint16_t start = rng.getRnd();//anfangswert
+  uint16_t periode = 1;
+  for (; rng.getRnd() != start; periode++) {}//bricht ab wenn Zufallsreihe wieder am Anfang ankommt
   return periode;
 }
 
@@ -84,6 +84,7 @@ int testPeriod(XORshift& rng){
 int main() {
   uint32_t anzahlWerte = 1e5;
 
+  // LCG Generatoren mit verschiedenen Parametern
   LCG erster(1234, 20, 120, 6075);
   abspeichern("build/ersterGenerator.txt", erster, anzahlWerte);
 
@@ -96,17 +97,27 @@ int main() {
   LCG vierter(1234, 16807, 0, 2147483647);
   abspeichern("build/vierterGenerator.txt", vierter, anzahlWerte);
 
+
+  //Aufgabe d
   XORshift fifth(11, 1, 7, 123);
   abspeichern("build/fifthGenerator.txt", fifth, anzahlWerte);
 
   XORshift six(11, 4, 7, 123);
   abspeichern("build/sixGenerator.txt", six, anzahlWerte);
 
-  for (int b = 1; b <= 15; b++) {
-    for (int c = 0; c <= 15; c++) {
-      XORshift seveneleven(11, b, c, 123);
-      cout << "testPeriod = " << testPeriod(seveneleven) << endl;
+  //Aufgabe e
+  ofstream otherfile;
+  otherfile.open("build/Rekursionstest.txt");
+  otherfile << "#b c periode \n";
+  for (int b = 1; b <= 15; b++) {//zwei Schleifen um b und c zu variieren
+    for (int c = 1; c <= 15; c++) {
+      XORshift seveneleven(11, b, c, 123);//XORshift mit entsprechendem b und c initialisieren
+      otherfile << testPeriod(seveneleven) << "\t";//bei festem b alle Perioden für
+      // die verschiedenen c in eine Zeile der txt Datei schreiben 
     }
+    otherfile << endl;//neue Zeile mit neuem b. Muss so gemacht werden um in der
+    //python Datei ein 2d-Array zu erhalten, welches für pcolormesh gebraucht wird
   }
+  otherfile.close();
 	return 0;
 }
--- Blatt2/aufgabe1/plot.py
+++ Blatt2/aufgabe1/plot.py
@@ -2,39 +2,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit, fmin
 import uncertainties.unumpy as unp
+import matplotlib.colors as colors
 
 
-def plotHisto(rnd, dateiname, m):
+def plotHistoScatter(rnd, dateiname, m):
 
+    # Histogramm erstellen
     plt.hist(rnd, bins=10)
     plt.xlabel(r'$bin$')
     plt.ylabel(r'$Häufigkeit$')
-    # in matplotlibrc leider (noch) nicht möglich
     plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
     plt.savefig("build/"+dateiname+"Hist.pdf")
     plt.close()
 
+    # Datenpaare plotten
+    # rnd[:min(m, 100000)-1] geht vom ersten bis zum vorletzten
+    # rnd[1:min(m, 100000)] geht vom zweiten bis zum letzten
     plt.scatter(rnd[:min(m, 100000)-1], rnd[1:min(m, 100000)])
     plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
     plt.savefig("build/"+dateiname+"Scatter.pdf")
     plt.close()
 
 
+def plotRekursion(periode):
+    # Farbplot erstellen um Periode der b,c Kombi zun untersuchen
+    x = np.arange(0.5, 16.5)  # dafür sorgen, dass die Zahlen in der Mitte der Kästchen sind
+    plt.pcolormesh(x, x, periode,
+                   norm=colors.LogNorm())  # beide Koordinaten, dann periode
+    # logarithmische Farbskala sorgt für bessere Lesbarkeit
+    plt.colorbar(label='Periode')
+    plt.xlabel('b')
+    plt.ylabel('c')
+    plt.savefig("build/rekursionstest.pdf")
+    plt.close()
+
+
 if __name__ == '__main__':
+    # LCG
     rnd_1 = np.genfromtxt('build/ersterGenerator.txt', unpack='True')
-    plotHisto(rnd_1, "erster", 6075)
+    plotHistoScatter(rnd_1, "erster", 6075)
 
     rnd_2 = np.genfromtxt('build/zweiterGenerator.txt', unpack='True')
-    plotHisto(rnd_2, "zweiter", 256)
+    plotHistoScatter(rnd_2, "zweiter", 256)
 
     rnd_3 = np.genfromtxt('build/dritterGenerator.txt', unpack='True')
-    plotHisto(rnd_3, "dritter", 2147483648)
+    plotHistoScatter(rnd_3, "dritter", 2147483648)
 
     rnd_4 = np.genfromtxt('build/vierterGenerator.txt', unpack='True')
-    plotHisto(rnd_4, "vierter", 2147483647)
+    plotHistoScatter(rnd_4, "vierter", 2147483647)
 
+    # XORshift
     rnd_5 = np.genfromtxt('build/fifthGenerator.txt', unpack='True')
-    plotHisto(rnd_5, "fifth", 100000)
+    plotHistoScatter(rnd_5, "fifth", 100000)
 
     rnd_6 = np.genfromtxt('build/sixGenerator.txt', unpack='True')
-    plotHisto(rnd_6, "six", 100000)
+    plotHistoScatter(rnd_6, "six", 100000)
+
+    # Periode untersuchen
+    periode = np.genfromtxt('build/Rekursionstest.txt', unpack='True')
+    plotRekursion(periode)
