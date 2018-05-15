8b8aa140ce54ae43af6bf3d863b63b9d8247e5c8
Jasper Lammering <jlammering@me.com>
2018-05-15 16:00:40 +0200

    bis a

--- Blatt3/aufgabe2/aufgabe2.cpp
+++ Blatt3/aufgabe2/aufgabe2.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <random>
 #include <fstream>
+#include <string>
 
 
 using namespace std;
@@ -20,7 +21,7 @@ public:
         if (drand48()<0.5)
           gitter_array[i][k] = +1;
         else
-          gitter_array[i][k] = -1;
+          gitter_array[i][k] = +1;
       }
     }
   }
@@ -43,67 +44,90 @@ public:
       for (int j = 0; j < gg; j++) {//zeilen
         for (int l = 0; l < gg; l++) {//spalten
         energie_berechnet +=
-          (getEntry(j, l)*getEntry(((j+1)%gg+gg)%gg, l) +
-           getEntry(j, l)*getEntry(((j-1)%gg+gg)%gg, l) +
-           getEntry(j, l)*getEntry(j, ((l-1)%gg+gg)%gg) +
-           getEntry(j, l)*getEntry(j, ((l+1)%gg+gg)%gg));
+          getEntry(j, l)*(getEntry(((j+1)%gg+gg)%gg, l) +
+           getEntry(((j-1)%gg+gg)%gg, l) +
+           getEntry(j, ((l-1)%gg+gg)%gg) +
+           getEntry(j, ((l+1)%gg+gg)%gg));
       }
     }
 
     return -1*(double)energie_berechnet*0.5;//*0.5 wegen doppelter nächster Nachbarn
   }
+
+
+  double getEnergyDiff(int j, int l){
+    int spin_aktuell = getEntry(j, l);
+    int gg = getGitterGroesse();
+    double energie_lokal = spin_aktuell*(getEntry(((j+1)%gg+gg)%gg, l) +
+     getEntry(((j-1)%gg+gg)%gg, l) +
+     getEntry(j, ((l-1)%gg+gg)%gg) +
+     getEntry(j, ((l+1)%gg+gg)%gg));
+
+    return 2*energie_lokal;
+  }
 };
 
-void zweiDimensionenIsing(int schritte, double beta, string dateiname){
+gitter zweiDimensionenIsing(int schritte, double kbT, string dateiname){
   //Gitter initialisieren
   gitter grid;
   int gitterGroesse = grid.getGitterGroesse();
+  double beta = pow(kbT, -1);
+
+  //äqibrilierungsphase
+  ofstream energyfile;
+  energyfile.open("build/energie"+dateiname+".txt");
 
   //Metropolis
+  double energie_allgemein = grid.getEnergy();
   for (int i = 0; i < schritte; i++) {
+    energyfile << energie_allgemein / pow(gitterGroesse, 2) << " " << i << "\n";
+
     int x = drand48()*gitterGroesse;
     int y = drand48()*gitterGroesse;
 
-    double energie_aktuell = grid.getEnergy();
-    grid.changeEntry(x, y);
-    double energie_nach_wechsel = grid.getEnergy();
-    double energiediff = energie_nach_wechsel - energie_aktuell;
+    double energiediff = grid.getEnergyDiff(x, y);
     //wieder ändern wenn es verworfen wird
-    if(energiediff < 0){
-      // wenn alte energie groesser als neue -> aktzeptiere neuen zustand
+    if(energiediff < 0){// wenn alte energie groesser als neue -> aktzeptiere neuen zustand
+      grid.changeEntry(x, y);
+      energie_allgemein += energiediff; //gesamtenergie ändert sich
     }else{ // wenn neue energie groesser -> erzeuge gleichverteilte zufallszahl auf [0,1] und vergleiche mit boltzmannfaktor
       double zufallszahl = drand48();
       double boltzmann = exp(-beta*energiediff);
-      //cout << energiediff << ' '<<zufallszahl<<" "<<boltzmann<<endl;
-      if (zufallszahl<boltzmann) { // wenn boltzmannfaktor groesser aktzeptiere
-      }else
-        grid.changeEntry(x, y);//zurück wechseln weil neuer Vorschlag verworfen wird
+      if (zufallszahl<boltzmann)
+        grid.changeEntry(x, y); // wenn boltzmannfaktor groesser aktzeptiere
+        energie_allgemein += energiediff;
     }
-
-
-
   }
+  energyfile.close();
 
 
   //ausgabe
-  ofstream myfile;
-    myfile.open(dateiname);
+  ofstream matrix;
+    matrix.open("build/"+dateiname+".txt");
      for (int i = 0; i < gitterGroesse; i++) {//zeilen
        for (int k = 0; k < gitterGroesse; k++) {//spalten
-         myfile << grid.getEntry(i, k) << " ";
+         matrix << grid.getEntry(i, k) << " ";
        }
-    myfile << "\n";
+    matrix << "\n";
      }
-    myfile.close();
+    matrix.close();
+  return(grid);
   }
+
+
 
 
 
 int main() {
   cout << "its something" << endl;
-  int schritte = 10000;
-  int kbT = 1;
-  double beta = 1/((double)kbT);
-  zweiDimensionenIsing(schritte, beta, "build/kbT=1.txt");
+  int schritte = 100000;
+  double kbTgleicheins = 1;
+  double kbTgleichzwei = 2;
+  double kbTgleichdrei = 3;
+
+  gitter g1 = zweiDimensionenIsing(schritte, kbTgleicheins, "kbTgleicheins");
+  gitter g2 = zweiDimensionenIsing(schritte, kbTgleichzwei, "kbTgleichzwei");
+  gitter g3 = zweiDimensionenIsing(schritte, kbTgleichdrei, "kbTgleichdrei");
+
   return 0;
 }
--- Blatt3/aufgabe2/plot.py
+++ Blatt3/aufgabe2/plot.py
@@ -13,7 +13,26 @@ def plotMatrix(matrix, dateiname):
     plt.close()
 
 
+def plotEnergie(energie, schritt, dateiname):
+    plt.plot(schritt, energie)
+    plt.xlabel("schritt")
+    plt.ylabel("energie")
+    plt.savefig("build/energie"+dateiname+".pdf")
+    plt.close()
+
 if __name__ == '__main__':
     # kbT = 1
-    matrix = np.genfromtxt('build/kbT=1.txt', unpack='True')
-    plotMatrix(matrix, 'kbTgleicheins')
+    matrix1 = np.genfromtxt('build/kbT=1.txt', unpack='True')
+    plotMatrix(matrix1, 'kbTgleicheins')
+    matrix3 = np.genfromtxt('build/kbT=3.txt', unpack='True')
+    plotMatrix(matrix3, 'kbTgleichdrei')
+
+    # energie
+    energie1, schritte1 = np.genfromtxt('build/energiekbTgleicheins.txt', unpack='True')
+    plotEnergie(energie1, schritte1, 'kbTgleicheins')
+
+    energie2, schritte2 = np.genfromtxt('build/energiekbTgleichzwei.txt', unpack='True')
+    plotEnergie(energie2, schritte2, 'kbTgleichzwei')
+
+    energie3, schritte3 = np.genfromtxt('build/energiekbTgleichdrei.txt', unpack='True')
+    plotEnergie(energie3, schritte3, 'kbTgleichdrei')
