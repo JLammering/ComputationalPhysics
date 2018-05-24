ad33e48b24d297c2d96fbbab6881ae84d9d2c7ce
Jasper Lammering <jlammering@me.com>
2018-05-24 14:26:02 +0200

    gitter_array geändert mit neuem struct

--- Blatt4/aufgabe1/aufgabe1.cpp
+++ Blatt4/aufgabe1/aufgabe1.cpp
@@ -10,14 +10,32 @@
 #include <fstream>
 #include <string>
 #include <cmath>
-#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+struct gitterplatz{
+private:
+	int eintrag;
+	bool besucht;
+public:
+	gitterplatz(){
+		eintrag = 0;
+		besucht = false;
+	}
+	void set_eintrag(int eintrag_setting){eintrag = eintrag_setting;}
+	void set_besucht(bool besucht_setting){besucht = besucht_setting;}
+	int get_eintrag(){return eintrag;}
+	bool get_besucht(){return besucht;}
+
+}
 
 
 class gitter{
   private:
     int gitterGroesse = 100;
-    int gitter_array[100][100];
+    gitterplatz gitter_array[100][100];
 public:
   gitter(int orientierungen){//Konstruktor
 		double wkeit = 1.0 / (double)orientierungen;
@@ -26,7 +44,7 @@ public:
         double rnd = drand48();
         for( int i_orient = 0; i_orient < orientierungen; i_orient++){
 					if ((rnd >  (i_orient) * wkeit) && (rnd <  (i_orient + 1) * wkeit)){
-	          gitter_array[i][k] = i_orient;
+	          gitter_array[i][k].set_eintrag(i_orient);
 					}
 				}
       }
@@ -38,36 +56,44 @@ public:
   }
 
   int getEntry(int x, int y){
-    return gitter_array[(x % gitterGroesse + gitterGroesse)%gitterGroesse][(y % gitterGroesse + gitterGroesse)%gitterGroesse];
+    return gitter_array[(x % gitterGroesse + gitterGroesse)%gitterGroesse][(y % gitterGroesse + gitterGroesse)%gitterGroesse].get_eintrag();
   }
 
-  void changeEntry(int x, int y){
-    gitter_array[x][y] *= -1;
+  void changeEntry(int x, int y){//bisher nur für q=2 implementiert
+    gitter_array[x][y].set_eintrag(gitter_array[x][y].get_eintrag()*=-1);
   }
 
+	void setBesucht(int x, int y){
+		gitter_array[x][y].set_besucht(true);
+	}
 
-  double getEnergyDiff(int j, int l){
-    int spin_aktuell = getEntry(j, l);
-    int gg = getGitterGroesse();
-    double energie_lokal = spin_aktuell*(getEntry(((j+1)%gg+gg)%gg, l) +
-     getEntry(((j-1)%gg+gg)%gg, l) +
-     getEntry(j, ((l-1)%gg+gg)%gg) +
-     getEntry(j, ((l+1)%gg+gg)%gg));
+	bool getBesucht(int x, int y){
+		return gitter_array[x][y].get_besucht();
+	}
 
-    return 2*energie_lokal;
-  }
+	//
+  // double getEnergyDiff(int j, int l){
+  //   int spin_aktuell = getEntry(j, l);
+  //   int gg = getGitterGroesse();
+  //   double energie_lokal = spin_aktuell*(getEntry(((j+1)%gg+gg)%gg, l) +
+  //    getEntry(((j-1)%gg+gg)%gg, l) +
+  //    getEntry(j, ((l-1)%gg+gg)%gg) +
+  //    getEntry(j, ((l+1)%gg+gg)%gg));
+	//
+  //   return 2*energie_lokal;
+  // }
 
 
-  double getMagnetisierung(){
-    int gg = getGitterGroesse();
-    int sum = 0;
-    for (int i = 0; i < gg; i++) {
-      for (int k = 0; k < gg; k++) {
-        sum += getEntry(i,k);
-      }
-    }
-    return ((double)sum/pow(gg, 2));
-  }
+  // double getMagnetisierung(){
+  //   int gg = getGitterGroesse();
+  //   int sum = 0;
+  //   for (int i = 0; i < gg; i++) {
+  //     for (int k = 0; k < gg; k++) {
+  //       sum += getEntry(i,k);
+  //     }
+  //   }
+  //   return ((double)sum/pow(gg, 2));
+  // }
 
 
   void absaven(string dateiname){
@@ -87,53 +113,63 @@ struct position{
 private:
   int x;
   int y;
+	int gg;
 public:
-  position(int x_setting, int y_setting){
+  position(int x_setting, int y_setting, int gg_setting){
     x= x_setting;
     y = y_setting;
+		gg = gg_setting;
   }
   int get_x(){ return x;}
   int get_y(){ return y;}
   void setting(int x_setting, int y_setting) {
-    x = x_setting;
-    y = y_setting;
+    x = (x_setting%gg+gg)%gg;
+    y = (y_setting%gg+gg)%gg;
   }
 };
 
 
 
 void wolffAlgorithmus(gitter& grid, int schritte, double kbT){
-  int gitterGroesse = grid.getGitterGroesse();
+  int gg = grid.getGitterGroesse();
   double beta = 1.0/kbT;
   double J = 1;
   double p_C = 1-exp(-2.0*beta*J);
 
   for (size_t i = 0; i < schritte; i++) {
-    double x = drand48();
-    double y = drand48();
-    stack<position> unbesuchteSpins;
-    stack<position> cluster;
+    double anfangs_x = drand48()*gg;
+    double anfangs_y = drand48()*gg;
 
-    position ersterSpin(x, y);
+    queue<position> unbesuchteSpins;
+    queue<position> cluster;
+
+    position ersterSpin(anfangs_x, anfangs_y, gg);
     unbesuchteSpins.push(ersterSpin);
+		cluster.push(ersterSpin);
+    int richtung = grid.getEntry(anfangs_x, anfangs_y);
 
-    int richtung = grid.getEntry(x, y);
+    while (!unbesuchteSpins.empty()) {
+    position aktuell = unbesuchteSpins.front();
 
-    while (unbesuchteSpins.size() > 0) {
-    position aktuell = unbesuchteSpins.back();
-    position nachbar(-1,-1);
+		unbesuchteSpins.pop();
+
+		x = aktuell.get_x();
+		y = aktuell.get_y();
+    position nachbar(-1,-1, gg);
+
 
     for (int i = -1; i <= 1; i++) { // erzeuge alle moeglichen Schritte um NN zu erreichen
       for (int j = -1; j <= 1; j++) {
         if (abs(i + j) == 1) {
-          if(drand48() < p_C){
-            nachbar.setting(x + i, y + j);
+          if(!grid.getBesucht(x+i, y+j) && richtung == grid.getEntry(x+i, y+j) && drand48() < p_C){//wenn er noch nicht besucht wurde, die richtung die ist vom initialen Spin und die Wkeit stimmt:
+            nachbar.setting((x+i), (y+j));
             cluster.push(nachbar);
             unbesuchteSpins.push(nachbar);
           }
         }
       }
     }
+		grid.setBesucht(x, y);//den gitterplatz markieren, dass er besucht wurde
   }
 
   }//schritte Schleife ende
@@ -149,7 +185,7 @@ double b_crit(int q){
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
-  gitter grid(4);
+  gitter grid(2);
   grid.absaven("thisisatest");
   wolffAlgorithmus(grid, 1, 1);
 
