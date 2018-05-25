2a125e7788c4c3e23a24f7a7d793881744d4f812
Jasper Lammering <jlammering@me.com>
2018-05-25 17:45:49 +0200

    kakakakakakakakkakakakakakakakaka

--- Blatt4/aufgabe1/aufgabe1.cpp
+++ Blatt4/aufgabe1/aufgabe1.cpp
@@ -17,42 +17,23 @@
 
 using namespace std;
 
-struct gitterplatz{
-private:
-	int eintrag;
-	bool besucht;
-public:
-	gitterplatz(){
-		eintrag = 0;
-		besucht = false;
-	}
-	void set_eintrag(int eintrag_setting){eintrag = eintrag_setting;}
-	void set_besucht(bool besucht_setting){besucht = besucht_setting;}
-	int get_eintrag(){return eintrag;}
-	bool get_besucht(){return besucht;}
-
-};
-
 
 class gitter{
   private:
-    int gitterGroesse = 30;
-    gitterplatz gitter_array[30][30];
+    int gitterGroesse = 100;
+    int gitter_array[100][100];
 		int orientierungen;
-		deque<int> orientliste;
-
 public:
   gitter(int orientierungen_set){//Konstruktor
 		orientierungen = orientierungen_set;
-		for (int o_iter=0; o_iter<orientierungen; o_iter++) orientliste.push_back(o_iter);
 
 		double wkeit = 1.0 / (double)orientierungen;
 		for (int i = 0; i < gitterGroesse; i++) {//zeilen
       for (int k = 0; k < gitterGroesse; k++) {//spalten
         double rnd = drand48();
-        for( int i_orient = 0; i_orient < orientierungen; i_orient++){
+        for( int i_orient = 0; i_orient < orientierungen; i_orient++){ // initaliesiere gitter zufaellig mit verschiedenen Eintraegen.
 					if ((rnd >  (i_orient) * wkeit) && (rnd <  (i_orient + 1) * wkeit)){
-	          gitter_array[i][k].set_eintrag(i_orient);
+	          gitter_array[i][k] = i_orient;
 					}
 				}
       }
@@ -64,54 +45,12 @@ public:
   }
 
   int getEntry(int x, int y){
-    return gitter_array[(x % gitterGroesse + gitterGroesse)%gitterGroesse][(y % gitterGroesse + gitterGroesse)%gitterGroesse].get_eintrag();
+    return gitter_array[(x % gitterGroesse + gitterGroesse)%gitterGroesse][(y % gitterGroesse + gitterGroesse)%gitterGroesse];
   }
 
-  void changeEntry(int x, int y, int ziel){//bisher nur für q=2 implementiert
-	// orientliste.erase(getEntry(x, y));
-	// 			double rnd = drand48();
-	// 			for( int i_orient = 0; i_orient < orientierungen - 1; i_orient++){
-	// 				if ((rnd >  (i_orient) * 1/(orientierungen - 1)) && (rnd <  (i_orient + 1) * 1/(orientierungen - 1))){
-	// 					gitter_array[x][y].set_eintrag(orientliste.at(i_orient));
-	// 				}
-	// 			}
-	// orientliste.emplace(getEntry(x, y), getEntry(x,y));
-	gitter_array[x][y].set_eintrag(ziel);
-
-
-  }
-
-	void setBesucht(int x, int y, bool value){
-		gitter_array[x][y].set_besucht(value);
+  void changeEntry(int x, int y, int ziel){
+	gitter_array[(x % gitterGroesse + gitterGroesse)%gitterGroesse][(y % gitterGroesse + gitterGroesse)%gitterGroesse]= ziel;
 	}
-
-	bool getBesucht(int x, int y){
-		return gitter_array[(x % gitterGroesse + gitterGroesse)%gitterGroesse][(y % gitterGroesse + gitterGroesse)%gitterGroesse].get_besucht();
-	}
-
-	//
-  // double getEnergyDiff(int j, int l){
-  //   int spin_aktuell = getEntry(j, l);
-  //   int gg = getGitterGroesse();
-  //   double energie_lokal = spin_aktuell*(getEntry(((j+1)%gg+gg)%gg, l) +
-  //    getEntry(((j-1)%gg+gg)%gg, l) +
-  //    getEntry(j, ((l-1)%gg+gg)%gg) +
-  //    getEntry(j, ((l+1)%gg+gg)%gg));
-	//
-  //   return 2*energie_lokal;
-  // }
-
-
-  // double getMagnetisierung(){
-  //   int gg = getGitterGroesse();
-  //   int sum = 0;
-  //   for (int i = 0; i < gg; i++) {
-  //     for (int k = 0; k < gg; k++) {
-  //       sum += getEntry(i,k);
-  //     }
-  //   }
-  //   return ((double)sum/pow(gg, 2));
-  // }
 
 
   void absaven(string dateiname){
@@ -127,7 +66,7 @@ public:
   }
 };
 
-struct position{
+struct position{ // erzeuge struct das eine position auf dem gitter darstellt
 private:
   int x;
   int y;
@@ -135,89 +74,81 @@ private:
 public:
   position(int x_setting, int y_setting, int gg_setting){
 		gg = gg_setting;
-    x= (x_setting%gg+gg)%gg;
+    x= (x_setting%gg+gg)%gg; // periodische Randbedingungen
     y = (y_setting%gg+gg)%gg;
   }
   int get_x(){ return x;}
   int get_y(){ return y;}
-  void setting(int x_setting, int y_setting) {
-    x = (x_setting%gg+gg)%gg;
-    y = (y_setting%gg+gg)%gg;
-  }
+
 };
 
 
 
 void wolffAlgorithmus(gitter& grid, int schritte, double kbT, int q){
   int gg = grid.getGitterGroesse();
-  double beta = 1.0/kbT;
+  double beta = 1.0/kbT; // berechne beta
   double J = 1;
-  double p_C = 1-exp(-2.0*beta*J);
+  double p_C = 1-exp(-2.0*beta*J); //Wkeit zum Cluster hinzugefügt zu werden
 
   for (size_t t = 0; t < schritte; t++) {
-		grid.absaven("2DTESTRUN"	+ to_string(t));
+		grid.absaven(to_string(q)+"ZUSTAENDE_RUN"	+ to_string(t)); // speichere Gitter ab
 
 
-    int anfangs_x = drand48()*gg;
+
+    int anfangs_x = drand48()*gg;  // zufaellige startposition
     int anfangs_y = drand48()*gg;
 
-    queue<position> unbesuchteSpins;
-    queue<position> cluster;
+    queue<position> unbesuchteSpins; // schlange fuer unbesuchteSpins
 
-    position ersterSpin(anfangs_x, anfangs_y, gg);
-    unbesuchteSpins.push(ersterSpin);
-		cluster.push(ersterSpin);
-    int richtung = grid.getEntry(anfangs_x, anfangs_y);
+    position ersterSpin(anfangs_x, anfangs_y, gg); // erzeuge position mit der startposition
+    unbesuchteSpins.push(ersterSpin); // fuege diese erste position den unbesuchten Spins hinzu
+    int richtung = grid.getEntry(anfangs_x, anfangs_y); // speichere die ausrichtung des ausgewaehlten spins
+		int zielflip = (int)(richtung + (drand48()*(q-1)+1)) % q;//ergibt zufällig eine der anderen verbleibenden Orientierungen
 
-    while (!unbesuchteSpins.empty()) {
 
-    position aktuell = unbesuchteSpins.front();
+    while (!unbesuchteSpins.empty()) { // fuehre so lange aus, wie es noch unbesuchte spins im cluster gibt
+		if (unbesuchteSpins.size()>gg*gg) {//ist irgendwie nötig weil sonst zu viele mitgenommen werden
+			break;
+		}
+
+    position aktuell = unbesuchteSpins.front();//unbesuchteSpins durchgehen dessen Nachbarn checken
 		int x = aktuell.get_x();
 		int y = aktuell.get_y();
-		unbesuchteSpins.pop();
+		unbesuchteSpins.pop();//rauslöschen
 
 
     for (int i = -1; i <= 1; i++) { // erzeuge alle moeglichen Schritte um Nachbarn zu erreichen
       for (int j = -1; j <= 1; j++) {
-        if (abs(i + j) == 1) {//nur NN
+        if (abs(i + j) == 1) {//nur Naechste Nachbarn
 					double rnd = drand48();
-					cout << (!grid.getBesucht(x+i, y+j)) << (richtung == grid.getEntry(x+i, y+j)) << (rnd < p_C) << endl;
-          if(!grid.getBesucht(x+i, y+j) && richtung == grid.getEntry(x+i, y+j) && rnd < p_C){//wenn er noch nicht besucht wurde, die richtung die ist vom initialen Spin und die Wkeit stimmt:
-						cout << "guter Nachbar gefunden" << endl;
-						position nachbar((x+i), (x+j), gg);
-            cluster.push(nachbar);
-            unbesuchteSpins.push(nachbar);
+          if((richtung == grid.getEntry(x+i, y+j)) && (drand48() < p_C)){//wenn die richtung die gleiche wie beim initialen Spin ist und die Wkeit stimmt:
+						position nachbar((x+i), (y+j), gg); // erzeuge nachbarposition
+						grid.changeEntry(x+i, y+j, zielflip); // flip jeden cluster eintrag einzlend
+            unbesuchteSpins.push(nachbar); //den nachbar zu den unbesuchteSpins hinzufügen um dann seine Nachbarn durchgehen zu können
           }
         }
       }
     }
-		grid.setBesucht(x, y, true);//den gitterplatz markieren, dass er besucht wurde
-		cout << "cluster.size()=" << cluster.size() << "unbesuchteSpins.size()=" << unbesuchteSpins.size()<<endl;
   } //unbesuchteSpins Schleife
-	cout << "cluster gebildet" << "mit größe"<<cluster.size()<<endl;
-	int zielflip = (int)(richtung + (drand48()*(q-1)+1)) % q;//nochmal drüber nachdenken
-	cout << "cluster flippen von " << richtung << " nach " << zielflip<< endl;
-	while(!cluster.empty()) {//cluster flippen
-		cout << "position" <<  cluster.front().get_x()<< cluster.front().get_y() << endl;
-		grid.changeEntry(cluster.front().get_x(), cluster.front().get_y(), zielflip);
-		grid.setBesucht(cluster.front().get_x(), cluster.front().get_y(), false);
-		cluster.pop();
-	}
   } //schritte Schleife end
 }
 
 
-double b_crit(int q){
-    return(log(1 + sqrt(q)));
+double kbT_crit(int q){  // berechnet den kritischen Wert fuer kbT
+    return(1 / log(1 + sqrt(q)));
 }
 
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
-	int q = 2; // anzahl
-  gitter grid(q);
-  wolffAlgorithmus(grid, 2, 1, q);
+	int q1 = 2; // anzahl verschiedener einstellmoeglichkeiten
+	int q2 = 3;
+  gitter grid1(q1); // erzeuge gitter mit spins
+	gitter grid2(q2);
+  wolffAlgorithmus(grid1, 100, 1, q1);
+	wolffAlgorithmus(grid2, 100, 1, q2);
+
+
 
 	return 0;
 }
--- Blatt4/aufgabe1/plot.py
+++ Blatt4/aufgabe1/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 def plotMatrix(matrix, dateiname):
@@ -10,10 +11,35 @@ def plotMatrix(matrix, dateiname):
     plt.savefig("build/plots/" + dateiname + ".pdf")
     plt.close()
 
-
 if __name__ == '__main__':
 
-    anzahl_steps = 10
-    for i in range(anzahl_steps):
-        mat = np.genfromtxt("build/2DTESTRUN"+str(i)+".txt", unpack='True')
-        plotMatrix(mat, "2DTESTRUN"+str(i))
+
+    anzahl_steps = 100
+    # for i in range(anzahl_steps):
+    #     mat = np.genfromtxt("build/2DTESTRUN"+str(i)+".txt", unpack='True')
+    #     plotMatrix(mat, "2DTESTRUN"+str(i))
+
+
+def update_fig1(steps):
+        steps *= 1
+        matrix = np.genfromtxt("build/2ZUSTAENDE_RUN"+str(steps)+".txt", unpack='True')
+        x = np.arange(0, len(matrix))
+        frame = plt.pcolormesh(x, x, matrix)
+        plt.title('sweep nr:' +str(steps))
+        # plt.colorbar(label='Spin')
+        return(frame)
+
+def update_fig2(steps):
+        steps *= 1
+        matrix = np.genfromtxt("build/3ZUSTAENDE_RUN"+str(steps)+".txt", unpack='True')
+        x = np.arange(0, len(matrix))
+        frame = plt.pcolormesh(x, x, matrix)
+        plt.title('sweep nr:' + str(steps))
+        # plt.colorbar(label='Spin')
+        return(frame)
+
+
+ani1 = animation.FuncAnimation(plt.figure(), update_fig1, frames=anzahl_steps)
+plt.show()
+ani2 = animation.FuncAnimation(plt.figure(), update_fig2, frames=anzahl_steps)
+plt.show()
