f4f9be4fa6a99241c2a9b4082a618050d2e8964e
Jasper Lammering <jlammering@me.com>
2018-06-05 13:54:02 +0200

    krass viel gemacht

---
+++ Blatt5/aufgabe2/aufgabe2.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : aufgabe2.cpp
+// Author      : Henning Ptaszyk, Jasper Lammering
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <Eigen/Eigenvalues>
+using namespace std;
+using namespace Eigen;
+
+void heisenbergModell(int N){
+  cout << N << endl;
+  MatrixXd hmat(6, 6);
+
+}
+
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+  heisenbergModell(4);
+	return 0;
+}
